9d6f25f83806c7ff27bc6940a85c4005dbce6b30
ios10 <trikshotzz05@gmail.com>
2022-06-27 03:27:58 -0700

    422 left

--- app/models/animal.rb
+++ app/models/animal.rb
@@ -3,5 +3,5 @@ class Animal < ApplicationRecord
     validates_presence_of :name
     validates_presence_of :latin_name
     validates_presence_of :kingdom
-    
+    validates :name, uniqueness: true
 end
